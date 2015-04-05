330d2e7f82ace4cb8e12f6b260d1737f8742bce3
unknown <moataz.elbarkouky@mail.utoronto.ca>
2015-04-04 21:41:44 -0700

    Sexy beast adding project

---
+++ DoctorsTravellers/Controllers/HomeController.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace DoctorsTravellers.Controllers
+{
+    public class HomeController : Controller
+    {
+        //
+        // GET: /Home/
+
+        //HOME page
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        //RESULT page
+        public ActionResult Result()
+        {
+            return View();
+        }
+
+    }
+}
