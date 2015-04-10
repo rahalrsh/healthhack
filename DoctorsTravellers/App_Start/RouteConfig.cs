1b016a980b616c983dccd986fb3be0d1f1168ddb
rahalrsh <rahalrsh@gmail.com>
2015-04-09 20:57:11 -0400

    adding basic structure for Sign Up and Login

--- DoctorsTravellers/App_Start/RouteConfig.cs
+++ DoctorsTravellers/App_Start/RouteConfig.cs
@@ -17,6 +17,7 @@ namespace DoctorsTravellers
                 name: "Default",
                 url: "{controller}/{action}/{id}",
                 defaults: new { controller = "Home", action = "Index", id = UrlParameter.Optional }
+                //defaults: new { controller = "Login", action = "MainPage", id = UrlParameter.Optional }
             );
         }
     }
--- DoctorsTravellers/Models/Config.cs
+++ DoctorsTravellers/Models/Config.cs
@@ -9,6 +9,6 @@ namespace DoctorsTravellers.Models
     public static class Config
     {
        //public static string MyConnectionString = "server=localhost; database=healthdevdb; uid=root; pwd=sqlroot;";
-        public static string MyConnectionString = "server=54.68.24.119; database=healthdevdb; uid=root; pwd=sqlroot;";
+       public static string MyConnectionString = "server=54.68.24.119; database=healthdevdb; uid=root; pwd=sqlroot;";
     }
 }
